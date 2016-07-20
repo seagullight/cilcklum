f4e53837541bd2a5f1434cc54afdac59c353194c
Andrii Chaika <seagullight@gmail.com>
2016-07-20 19:13:55 +0300

    section with toggle table

--- js/common.js
+++ js/common.js
@@ -4,6 +4,19 @@ $(document).ready(function() {
 	 $("#sandwich").toggleClass("active");
 	 $("#navigat").css({"left":0}).toggle(1000);
 });
-	
+		
+		$("button.btn-default").hover(function(){
+				$(this).animate({backgroundColor: "blue"}, 1000);}, function(){
+				$(this).animate({backgroundColor: "transparent"});
+		});
+
+		$("div.suli").html($("li.inft").next().html());
+		$("li.inft").click(function(){
+			$(this).toggleClass("active").siblings().removeClass("active");
+			$("div.suli").html($(this).next().html());
+
+		});	
+		
+		
 
 		   });
