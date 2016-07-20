4eb75bf4016fdda86756c1e628e27e45e200eb66
Andrii Chaika <seagullight@gmail.com>
2016-07-20 16:57:36 +0300

    madia plus toggle button

--- js/common.js
+++ js/common.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-	
+	$("#sandwich, .menu_item").click(function() {
 
+	 $("#sandwich").toggleClass("active");
+	 $("#navigat").css({"left":0}).toggle(1000);
+});
+	
 
 		   });
