ee4fe3e02006e6ae25a56202e215a60bec94812a
Andrii Chaika <seagullight@gmail.com>
2016-07-21 17:00:44 +0300

    with section succsec

--- js/common.js
+++ js/common.js
@@ -5,18 +5,37 @@ $(document).ready(function() {
 	 $("#navigat").css({"left":0}).toggle(1000);
 });
 		
-		$("button.btn-default").hover(function(){
-				$(this).animate({backgroundColor: "blue"}, 1000);}, function(){
-				$(this).animate({backgroundColor: "transparent"});
-		});
+		
+		
+				
+		
 
+		
 		$("div.suli").html($("li.inft").next().html());
 		$("li.inft").click(function(){
 			$(this).toggleClass("active").siblings().removeClass("active");
 			$("div.suli").html($(this).next().html());
 
-		});	
-		
-		
+$("a.btnwrap").hover(function(){
+			$(this).find("span").animate({width:"100%"},500).text($(this).text());}, function(){
+			$(this).find("span").animate({width:"0"},500).text("");
+		});
 
+		});	
+$("a.btnwrap").hover(function(){
+			$(this).find("span").animate({width:"100%"},500).text($(this).text());}, function(){
+			$(this).find("span").animate({width:"0"},500).text("");
+		});		
+		
+$("div.pick, div.pick2 ").mouseover(function(){
+$(this).find(".supick").css({"display": "block"});
+});
+$(".supick").mouseout(function(){
+$(this).css({"display":"none"});
+});
+$(".supick a").hover(function(){
+$(this).find("span").animate({width: "60%"}, 300).text($(this).text()); }, function(){
+	$(this).find("span").animate({width: "0"}, 300).text("");
+
+});		
 		   });
